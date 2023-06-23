0bebd20e5d400d506d53278c641718b36ba3681c
Timothy Luk <212979d@mymail.nyp.edu.sg>
2023-06-23 13:09:11 +0800

    Added occlusion base + made view class modular <3
    Yay !

---
+++ UnityProj/FYP_Main/Assets/Scripts/Minigame/Minigame Logic/Occlusion/OcclusionTaskController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Common.DesignPatterns;
+using UniRx.Extention;
+using UnityEngine.Events;
+using UniRx;
+
+
+public class OcclusionTaskController : MinigameTaskController<PeriLineSamTasks>
+{
+
+}
+
+// The tasks that need to be completed for this 
+public enum OcclusionTasks
+{
+    MUTE_ALARM,
+
+    // Optional tasks
+    // Scenario 1: Roller clamp
+    OPEN_ROLLER_CLAMP,
+
+    // Scenario 2: Kinked lines
+    UNKINK_LINE,
+
+    // Scenario 3: T-Connector
+    UNCLAMP_T_CONNECTOR,
+    FIX_3WAY_TAP,
+
+    // Scenario 4: No Phlebitis
+    ASSESS_SKIN,
+    RESTART_DRIP,
+
+    // Scenario 5: Phlebitis
+    CLAMP_T_CONNECTOR,
+    PUT_PUMP_ON_STANDBY,
+    INFORM_STAFF_NURSE,
+
+    DISCONNECT_TUBING_FROM_PLUG,
+    REMOVE_TEGADERM,
+    PULL_OUT_PLUG,
+    USE_GAUZE,
+    PUT_PLASTER,
+
+    NUM_MANDATORY_TASKS,
+
+    NUM_TASKS
+}
--- UnityProj/FYP_Main/Assets/Scripts/Minigame/Minigame Logic/Peripheral Line Sample Setup/PeriLineSamUIController.cs
+++ UnityProj/FYP_Main/Assets/Scripts/Minigame/Minigame Logic/Peripheral Line Sample Setup/PeriLineSamUIController.cs
@@ -8,75 +8,8 @@ using UniRx.Extention;
 using Common.DesignPatterns;
 
 // This class controls the UI system of the minigame
-public class PeriLineSamUIController : MonoBehaviour
+public class PeriLineSamUIController : UIViewController<PeriLineView>
 {
-    // TODO: change this to a generic class LMAO
-    #region UI variables
-    // Patient stuff
-    [SerializeField] private Image _patientSprite;
-
-    // Panels
-    private Stack<PeriLineView> _visitedPanels;
-    private PeriLineView _currentPanel;
-    [SerializeField] private List<PeriLinePanels> _viewPanels;
-    #endregion
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        // Init all other variables
-        _visitedPanels = new Stack<PeriLineView>();
-        _visitedPanels.Push(PeriLineView.PATIENT_VIEW);
-
-        ChangePanels(_visitedPanels.Peek());
-
-        // Generate a random patient
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    #region Panel Changes
-    private void ChangePanels(PeriLineView newView)
-    {
-        // Count if that panel even exists
-        if (_viewPanels.Where(s => s.PanelView == newView).Count() == 0) 
-        {
-            Debug.LogWarning("The current panel " + newView.ToString() + " you are trying to access does not exist.");
-            return;
-        }
-        
-        // Check set the current panel to false
-        var activePanels = _viewPanels.Where(s => s.PanelView == _currentPanel).Select(s => s.PanelObject);
-        foreach (var Panel in activePanels)
-        {
-            Panel.SetActive(false);
-        }
-
-        // Set the last instance of the wanted panel to true
-        _viewPanels.Where(s => s.PanelView == newView).Select(s => s.PanelObject).LastOrDefault().SetActive(true);
-
-    }
-
-    private void ChangePanelsDefault(PeriLineView newView)
-    {
-        ChangePanels(newView);  
-        _visitedPanels.Push(newView);
-        _currentPanel = newView;
-    }
-
-    public void Back()
-    {
-        _visitedPanels.Pop();
-        Debug.Log(_visitedPanels.Peek());
-        ChangePanels(_visitedPanels.Peek());
-        _currentPanel = _visitedPanels.Peek();
-    }
-    #endregion
-
     #region UI button callbacks
     public void ChangetoTray() { ChangePanelsDefault(PeriLineView.TRAY_DEFAULT); }
     public void ChangetoPatient() { ChangePanelsDefault(PeriLineView.PATIENT_VIEW); }
@@ -86,13 +19,6 @@ public class PeriLineSamUIController : MonoBehaviour
     #endregion
 }
 
-// This class gives an object for the panels
-[System.Serializable]
-public class PeriLinePanels 
-{
-    public PeriLineView PanelView;
-    public GameObject PanelObject;
-}
 
 // This enum marks which current panel it is currently on 
 public enum PeriLineView
@@ -100,7 +26,7 @@ public enum PeriLineView
     PATIENT_VIEW,
     TRAY_DEFAULT,
     IV_TUBE_CLOSEUP,
-    TRAY_,
+    TRAY,
 
     IV_BAG_CLOSEUP,
     IV_BAG_WITH_SPIKE_CLOSEUP,
