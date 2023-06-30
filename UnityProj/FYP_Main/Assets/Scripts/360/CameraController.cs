bb20a3efa94a2d2e71bc599f8855f0dba8c9c051
abzhikai <abzhikai@gmail.com>
2023-06-30 17:19:13 +0800

    360 Camera Script Reformat
    Changed the Scripts of the 360 Camera to follow the Design Pattern

--- UnityProj/FYP_Main/Assets/Scripts/360/CameraController.cs
+++ UnityProj/FYP_Main/Assets/Scripts/360/CameraController.cs
@@ -1,30 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Core.Input;
 
-public class CameraController : MonoBehaviour
+public class CameraController : MonoBehaviour, IInputActions
 {
     [SerializeField, Range(0, 50)]
     public float cameraSensitivity = 50;
     public float yaw;
     public float pitch;
+    private float minClamp = -90f;
+    private float maxClamp = 90f;
+    private Vector2 touch;
 
-    // Update is called once per frame
-    void Update()
+    void OnEnable()
+    {
+        InputManager.Instance.AddSubscriber(this);
+    }
+
+    void OnDisable()
+    {
+        if (InputManager.Instance != null)
+        {
+            InputManager.Instance.RemoveSubscriber(this);
+        }
+    }
+
+    public void OnStartTap()
+    {
+
+    }
+
+    public void OnTap()
     {
         if (Input.touchCount > 0)
         {
-            if (Input.GetTouch(0).phase == TouchPhase.Moved)
-            {
-                // Movement of the player
-                Vector2 touch = Input.GetTouch(0).deltaPosition;
-                yaw += -touch.x * cameraSensitivity * Time.deltaTime;
-                pitch += touch.y * cameraSensitivity * Time.deltaTime;
-                pitch = Mathf.Clamp(pitch, -90f, 90f);
-                Quaternion rotation = Quaternion.Euler(pitch, yaw, 0);
-                transform.rotation = rotation;
-            }
+            // This gets the player touch
+            touch = Input.GetTouch(0).deltaPosition;
+            yaw += -touch.x * cameraSensitivity * Time.deltaTime; // Yaw calculates the left-right movement 
+            pitch += touch.y * cameraSensitivity * Time.deltaTime; // Pitch calculates the up-down movement
+            // This limits the player's up down movement
+            pitch = Mathf.Clamp(pitch, minClamp, maxClamp);
+            // Input the values as rotation values
+            Quaternion rotation = Quaternion.Euler(pitch, yaw, 0);
+            transform.rotation = rotation;
         }
+    }
+
+    public void OnEndTap()
+    {
+
     }
 
 }
--- UnityProj/FYP_Main/Assets/Scripts/360/SceneSprite.cs
+++ UnityProj/FYP_Main/Assets/Scripts/360/SceneSprite.cs
@@ -1,23 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Core.Input;
 
-public class SceneSprite : MonoBehaviour
+public class SceneSprite : MonoBehaviour, IInputActions
 {
     public float yaw;
     public float pitch;
 
-    // Update is called once per frame
-    void Update()
+    void OnEnable()
+    {
+        InputManager.Instance.AddSubscriber(this);
+    }
+
+    void OnDisable()
+    {
+        if (InputManager.Instance != null)
+        {
+            InputManager.Instance.RemoveSubscriber(this);
+        }
+    }
+
+    public void OnStartTap()
+    {
+        
+    }
+
+    public void OnTap()
     {
         // Get the yaw and pitch of Camera
         float cameraYaw = Camera.main.GetComponent<CameraController>().yaw;
         float cameraPitch = Camera.main.GetComponent<CameraController>().pitch;
-        // set yaw and pitch of sprite to be negative of the camera's
+        // Set Yaw and Pitch of sprite to be negative of the camera's
         yaw = -cameraYaw;
         pitch = -cameraPitch;
-
+        // Input the values as rotation values
         Quaternion rotation = Quaternion.Euler(pitch, yaw, 0);
         transform.rotation = rotation;
+    }
+
+    public void OnEndTap()
+    {
+
     }
 }
