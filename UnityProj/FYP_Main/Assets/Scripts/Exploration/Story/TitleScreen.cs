eb16af28bb4f7fbd4f2c90a28b57aa825b91e2cf
Timothy Luk <212979d@mymail.nyp.edu.sg>
2023-07-31 03:54:01 +0800

    Touchups

--- UnityProj/FYP_Main/Assets/Scripts/Exploration/Story/TitleScreen.cs
+++ UnityProj/FYP_Main/Assets/Scripts/Exploration/Story/TitleScreen.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class TitleScreen : MonoBehaviour
 {
     Sequence _sequence;  
     [SerializeField] private UnityEvent _afterDisplayEvent;
+    [SerializeField] private Image _titleImage;
     FadeInOut _fade;
 
     private void Awake()
@@ -20,6 +22,7 @@ public class TitleScreen : MonoBehaviour
         _sequence = DOTween.Sequence();
         _fade = GetComponent<FadeInOut>();
         _fade.FadeIn();
+        _titleImage.color = new Color(1,1,1,0);
         StartCoroutine(DisplayTitle());
     }
 
@@ -28,6 +31,7 @@ public class TitleScreen : MonoBehaviour
         yield return new WaitForSeconds(_fade.GetFloatDuration());
         
         // do something
+        _sequence.Append(_titleImage.DOFade(1.0f, 1.0f));
 
         yield return _sequence.WaitForCompletion();
         yield return new WaitForSeconds(3.0f); // Temporary.
